d686f153c4bf24ee4c24eb82fe34a8c3605a3c56
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-09-11 14:29:05 +0530

    file commited

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,18 @@
+import "./list.scss"; 
+import Datatable from "../../components/datatable/Datatable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+       <Datatable/>
+      </div>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,63 @@
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+
+function New({ inputs, title }) {
+  const [file, setFile] = useState("");
+
+  const handleChange = (e) => {
+   setFile(e.target.files[0])
+  };
+
+  
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
+              alt=""
+            />
+          </div>
+
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={handleChange}
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {inputs.map((input, id) => (
+                <div className="formInput" key={id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default New;
